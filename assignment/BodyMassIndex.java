dd3e0f7c4afd74c6637a737cf7e16e759014c8ea
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2024-10-18 02:14:08 +0100

    Added screenshots

--- assignment/BodyMassIndex.java
+++ assignment/BodyMassIndex.java
@@ -14,7 +14,6 @@ public class BodyMassIndex{
     System.out.print("Enter your height (in metres): ");
     float height = input.nextFloat();
     double bmi = weight / Math.pow(height, 2);//(weight/height^2)
-    
     /*the BMI scale follows:
     (bmi > 29.9)"You are obese"
     (25.0 <= bmi <= 29.9)"You have over weight"
@@ -24,7 +23,6 @@ public class BodyMassIndex{
     if (bmi > 29.9){System.out.println("You are obese");}
     else if (bmi > 24.9){System.out.println("You are over weight");}
     else if (bmi > 18.49){System.out.println("You have normal weight");}
-    else {System.out.println("You are under weight");}
-    
+    else {System.out.println("You are under weight");} 
   }
 }
