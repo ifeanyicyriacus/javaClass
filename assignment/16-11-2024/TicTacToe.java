10c03bd09d368abe50a1073bdd7ea1c555cfdad5
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2024-11-22 14:43:06 +0100

    Tic Tac Toe upgrade

--- assignment/16-11-2024/TicTacToe.java
+++ assignment/16-11-2024/TicTacToe.java
@@ -1,33 +1,24 @@
 import java.util.Scanner;
 public class TicTacToe{
+  static final int NO_OF_CELLS_IN_BOARD = 9;
   static Scanner input = new Scanner(System.in);
+  static int row;
+  static int column;
+  static String token;
+  static String[][] ticTacToeBoard = {{" ", " ", " "}, {" ", " ", " "}, {" ", " ", " "}};
   
   public static void main(String[] args){
-    final int NO_OF_CELLS_IN_BOARD = 9;
-    String[][] ticTacToeBoard = {{" ", " ", " "}, {" ", " ", " "}, {" ", " ", " "}};
-    
-    int row;
-    int column;
-    String token;
     System.out.println("Tic-Tac-Toe Game");
     System.out.println("Enter Row(1-3) and column(1-3) number.\nGAME ON");
     
     for (int index = 1; index <= NO_OF_CELLS_IN_BOARD; index += 1){
       System.out.println(displayBoard(ticTacToeBoard));
       if (index % 2 == 0){
-        token = "X";
-        System.out.printf("Player %s: enter row >> ", token );
-        row = collectRowOrColumn();
-        System.out.printf("Player %s: enter column >> ", token );
-        column = collectRowOrColumn();
-        addToken(ticTacToeBoard, row, column, token );
+        token = "\033[1;31mX\033[0m";
+        collectNextMove(token);
       } else {
-        token = "O";
-        System.out.printf("Player %s: enter row >> ", token );
-        row = collectRowOrColumn();
-        System.out.printf("Player %s: enter column >> ", token );
-        column = collectRowOrColumn();
-        addToken(ticTacToeBoard, row, column, token );
+        token = "\033[1;32mO\033[0m";
+        collectNextMove(token);
       }
       
       if (index >= 5){
@@ -42,6 +33,14 @@ public class TicTacToe{
         }
       }
     }
+  }
+  
+  public static void collectNextMove(String token){
+    System.out.printf("Player %s: enter row >> ", token );
+    row = collectRowOrColumn();
+    System.out.printf("Player %s: enter column >> ", token );
+    column = collectRowOrColumn();
+    addToken(ticTacToeBoard, row, column, token );
   }
   
   public static int collectRowOrColumn(){
@@ -70,8 +69,7 @@ public class TicTacToe{
   public static void addToken(String[][] board, int row, int column, String token){
     if (board[row - 1][column - 1].equals(" "))
       board[row - 1][column - 1] = token;
-    else {
-      do{
+    else do{
         System.out.println("Position has been filled, try again!");
         System.out.printf("Player %s: enter row >> ", token);
         row = input.nextInt();
@@ -79,15 +77,15 @@ public class TicTacToe{
         column = input.nextInt();
         addToken(board, row, column, token);
       }while(board[row - 1][column - 1].equals(" "));
-    }
   }
   
   public static String displayBoard(String[][] board){
     return String.format("      col 1 col 2 col 3 %n     +-----+-----+-----+%nrow 1|  %s  |  %s  |  %s  |%n     +-----+-----+-----+%nrow 2|  %s  |  %s  |  %s  |%n     +-----+-----+-----+%nrow 3|  %s  |  %s  |  %s  |%n     +-----+-----+-----+%n",
     board[0][0], board[0][1], board[0][2], board[1][0], board[1][1], board[1][2], board[2][0], board[2][1], board[2][2]);
-  
-  
   }
+  
+  
+  
 }
 
 
