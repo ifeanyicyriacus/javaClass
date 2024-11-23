95107f6766508578eac597eab38fc2652e038119
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2024-11-23 10:28:34 +0100

    made code more modular, with single single responsiblity static method

--- assignment/16-11-2024/TicTacToe.java
+++ assignment/16-11-2024/TicTacToe.java
@@ -1,49 +1,62 @@
 import java.util.Scanner;
 public class TicTacToe{
-  static final int NO_OF_CELLS_IN_BOARD = 9;
-  static Scanner input = new Scanner(System.in);
-  static int row;
-  static int column;
-  static String token;
-  static String[][] ticTacToeBoard = {{" ", " ", " "}, {" ", " ", " "}, {" ", " ", " "}};
   
   public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    final int NO_OF_CELLS_IN_BOARD = 9;
+    final int MIN_NO_OF_MOVES_FOR_A_WIN = 5;
+    final String X = "  \033[1;31mX  \033[0m";
+    final String O = "  \033[1;32mO  \033[0m";
+    String[][] ticTacToeBoard = {{"", "", ""}, {"", "", ""}, {"", "", ""}};
+    String token;
+    
     System.out.println("Tic-Tac-Toe Game");
     System.out.println("Enter Row(1-3) and column(1-3) number.\nGAME ON");
     
     for (int index = 1; index <= NO_OF_CELLS_IN_BOARD; index += 1){
       System.out.println(displayBoard(ticTacToeBoard));
       if (index % 2 == 0){
-        token = "\033[1;31mX\033[0m";
-        collectNextMove(token);
+        token = X;
+        collectNextMove(ticTacToeBoard, token, input);
       } else {
-        token = "\033[1;32mO\033[0m";
-        collectNextMove(token);
+        token = O;
+        collectNextMove(ticTacToeBoard, token, input);
       }
       
-      if (index >= 5){
-        if (checkForWinner(ticTacToeBoard) == false && index == 9){
-          System.out.println(displayBoard(ticTacToeBoard));
-          System.out.println("GAME OVER\n DRAW!!!");
-          break;
-        }else if (checkForWinner(ticTacToeBoard) == true){
-          System.out.println(displayBoard(ticTacToeBoard));
-          System.out.printf("GAME OVER\n%s Player Wins\n", token );
-          break;
-        }
+      if (index >= MIN_NO_OF_MOVES_FOR_A_WIN && checkForWinner(ticTacToeBoard) == true){
+        System.out.println(displayBoard(ticTacToeBoard));
+        System.out.printf("GAME OVER\nPlayer-%s Wins\n", token );
+        break;
+      }else if (index == 9){
+        System.out.println(displayBoard(ticTacToeBoard));
+        System.out.println("GAME OVER\nDRAW!!!");
+        break;
       }
     }
+    
   }
   
-  public static void collectNextMove(String token){
-    System.out.printf("Player %s: enter row >> ", token );
-    row = collectRowOrColumn();
-    System.out.printf("Player %s: enter column >> ", token );
-    column = collectRowOrColumn();
-    addToken(ticTacToeBoard, row, column, token );
+  public static void collectNextMove(String[][] board, String token, Scanner input){
+    int retry = 0;
+    int row; int column;
+    do{
+      if (retry != 0){ System.out.println("Position has been filled, try again!"); }
+      System.out.printf("Player %s: enter row >> ", token );
+      row = collectRowOrColumn(input);
+      System.out.printf("Player %s: enter column >> ", token );
+      column = collectRowOrColumn(input);
+      retry += 1;
+    }while(!addToken(board, token, row, column));
+  }
+
+  public static boolean addToken(String[][] board, String token, int row, int column){
+    if (board[row - 1][column - 1].equals("")){
+      board[row - 1][column - 1] = token;
+      return true;
+    }else { return false; }
   }
   
-  public static int collectRowOrColumn(){
+  public static int collectRowOrColumn(Scanner input){
     int number = 0;
     do{
       if (number != 0){ System.out.print("Invalid input, try again!>> ");}
@@ -53,34 +66,44 @@ public class TicTacToe{
   }
   
   public static boolean checkForWinner(String[][] board){
-    if (!board[0][0].equals(" ") && board[0][0] == board[0][1] && board[0][1] == board[0][2]){ return true;}
-    else if (!board[1][0].equals(" ") && board[1][0] == board[1][1] && board[1][1] == board[1][2]){ return true;}
-    else if (!board[2][0].equals(" ") && board[2][0] == board[2][1] && board[2][1] == board[2][2]){ return true;}
+    if (!board[0][0].equals("") && board[0][0] == board[0][1] && board[0][1] == board[0][2]){
+      highlightWinCells(board, 0,0,0,1,0,2); 
+      return true; }
+    else if (!board[1][0].equals("") && board[1][0] == board[1][1] && board[1][1] == board[1][2]){
+      highlightWinCells(board, 1,0,1,1,1,2); 
+      return true; }
+    else if (!board[2][0].equals("") && board[2][0] == board[2][1] && board[2][1] == board[2][2]){
+      highlightWinCells(board, 2,0,2,1,2,2); 
+      return true; }   
+      
+    else if (!board[0][0].equals("") && board[0][0] == board[1][0] && board[1][0] == board[2][0]){
+      highlightWinCells(board, 0,0,1,0,2,0); 
+      return true; }
+    else if (!board[0][1].equals("") && board[0][1] == board[1][1] && board[1][1] == board[2][1]){
+      highlightWinCells(board, 0,1,1,1,2,1); 
+      return true; }
+    else if (!board[0][2].equals("") && board[0][2] == board[1][2] && board[1][2] == board[2][2]){
+      highlightWinCells(board, 0,2,1,2,2,2); 
+      return true; }
     
-    else if (!board[0][0].equals(" ") && board[0][0] == board[1][0] && board[1][0] == board[2][0]){ return true;}
-    else if (!board[0][1].equals(" ") && board[0][1] == board[1][1] && board[1][1] == board[2][1]){ return true;}
-    else if (!board[0][2].equals(" ") && board[0][2] == board[1][2] && board[1][2] == board[2][2]){ return true;}
-    
-    else if (!board[0][0].equals(" ") && board[0][0] == board[1][1] && board[1][1] == board[2][2]){ return true;}
-    else if (!board[0][2].equals(" ") && board[0][2] == board[1][1] && board[1][1] == board[2][0]){ return true;}
-    else { return false;} 
+    else if (!board[0][0].equals("") && board[0][0] == board[1][1] && board[1][1] == board[2][2]){
+      highlightWinCells(board, 0,0,1,1,2,2); 
+      return true; }
+    else if (!board[0][2].equals("") && board[0][2] == board[1][1] && board[1][1] == board[2][0]){
+      highlightWinCells(board, 0,2,1,1,2,0); 
+      return true; }
+    else { return false; } 
   }
   
-  public static void addToken(String[][] board, int row, int column, String token){
-    if (board[row - 1][column - 1].equals(" "))
-      board[row - 1][column - 1] = token;
-    else do{
-        System.out.println("Position has been filled, try again!");
-        System.out.printf("Player %s: enter row >> ", token);
-        row = input.nextInt();
-        System.out.printf("Player %s: enter column >> ", token);
-        column = input.nextInt();
-        addToken(board, row, column, token);
-      }while(board[row - 1][column - 1].equals(" "));
+  public static void highlightWinCells(String[][] board, int row1, int col1, int row2, int col2, int row3, int col3 ){
+    board[row1][col1] = "\033[47m" + board[row1][col1];
+    board[row2][col2] = "\033[47m" + board[row2][col2];
+    board[row3][col3] = "\033[47m" + board[row3][col3];
   }
+  
   
   public static String displayBoard(String[][] board){
-    return String.format("      col 1 col 2 col 3 %n     +-----+-----+-----+%nrow 1|  %s  |  %s  |  %s  |%n     +-----+-----+-----+%nrow 2|  %s  |  %s  |  %s  |%n     +-----+-----+-----+%nrow 3|  %s  |  %s  |  %s  |%n     +-----+-----+-----+%n",
+    return String.format("      col 1 col 2 col 3 %n     +-----+-----+-----+%nrow 1|%5s|%5s|%5s|%n     +-----+-----+-----+%nrow 2|%5s|%5s|%5s|%n     +-----+-----+-----+%nrow 3|%5s|%5s|%5s|%n     +-----+-----+-----+%n",
     board[0][0], board[0][1], board[0][2], board[1][0], board[1][1], board[1][2], board[2][0], board[2][1], board[2][2]);
   }
   
