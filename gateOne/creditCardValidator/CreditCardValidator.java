f5be909a784430bcb34bc1fa2890913873421497
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2024-12-06 11:31:04 +0100

    completed CreditCardValidator Tests cases

--- gateOne/creditCardValidator/CreditCardValidator.java
+++ gateOne/creditCardValidator/CreditCardValidator.java
@@ -5,45 +5,44 @@ public class CreditCardValidator{
   }
   
   
-  public static int getSumOfEvenPlacedDigit(int cardNumber){
-    String number = ""+cardNumber;
-    sum = 0;
-    for(int index = (number.length() - 2); index < 0 ; index -= 2){
-      int digit = Integer.valueOf(number.substring(index, index+1));
+  public static int getSumOfEvenPlacedDigitDoubled(String cardNumber){
+    int sum = 0;
+    for(int index = (cardNumber.length() - 2); index >= 0 ; index -= 2){
+      String digitString = cardNumber.substring(index, index+1);
+      int digit = Integer.valueOf(digitString);
       int digitDoubled = digit * 2;
-      sum += (digitDoubled >= 10)?sumDigitsInTwoDigitNumber(digitDoubled):digitDoubled
+      sum += (digitDoubled >= 10)?sumDigitsInTwoDigitNumber(digitDoubled):digitDoubled;
     }
     return sum;  
   }
   
-  public static int getSumOfOddPlacedDigit(int cardNumber){
-    String number = ""+cardNumber;
-    sum = 0;
-    for(int index = (number.length() - 1); index < 0 ; index -= 2){
-      int digit = Integer.valueOf(number.substring(index, index+1));
+  public static int getSumOfOddPlacedDigit(String cardNumber){
+    int sum = 0;
+    for(int index = (cardNumber.length() - 1); index >= 0 ; index -= 2){
+      String digitString = cardNumber.substring(index, index+1);
+      int digit = Integer.valueOf(digitString);
       sum += digit;
     }
     return sum;
   }
   
-  public static boolean isNumberCombinationCorrect(int cardNumber){
+  public static boolean isNumberCombinationCorrect(String cardNumber){
     int sumOfOddPlacedDigit = getSumOfOddPlacedDigit(cardNumber);
-    int sumOfEvenPlacedDigit = getSumOfEvenPlacedDigit(cardNumber);
-    int sum = sumOfOddPlacedDigit + sumOfEvenPlacedDigit;
+    int sumOfEvenPlacedDigitDoubled = getSumOfEvenPlacedDigitDoubled(cardNumber);
+    int sum = sumOfOddPlacedDigit + sumOfEvenPlacedDigitDoubled;
     return isNumberDivisibleBy10(sum);
   }
   
   
-  public static boolean isLengthValid(int cardNumber){
-    String number = ""+cardNumber;
-    return (number.length() >= 13) && (number.length() <= 16);
+  public static boolean isLengthValid(String cardNumber){
+    return (cardNumber.length() >= 13) && (cardNumber.length() <= 16);
   }
-  public static String returnCardType(int cardNumber){
-    String number = ""+cardNumber;
-    if (number.startsWith("4")){ return "Visa";}
-    else if (number.startsWith("5")){ return "MasterCard";}
-    else if (number.startsWith("37")){ return "American Express";}
-    else if (number.startsWith("6")){ return "Discover";}
+  public static String returnCardType(String cardNumber){
+    if (cardNumber.startsWith("4")){ return "Visa";}
+    else if (cardNumber.startsWith("5")){ return "MasterCard";}
+    else if (cardNumber.startsWith("37")){ return "American Express";}
+    else if (cardNumber.startsWith("6")){ return "Discover";}
+    else return "";
   }
   public static int sumDigitsInTwoDigitNumber(int number){
     return ((number / 10) + (number % 10));
--- gateOne/creditCardValidator/TestCreditCardValidator.java
+++ gateOne/creditCardValidator/TestCreditCardValidator.java
@@ -2,16 +2,77 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestCreditCardValidator{
+  String sample1 = "4388576018402626";//invalid
+  String sample2 = "5399831619690403";//valid
+  String sample3 = "5399831619690404";//invalid
+  String sample4 = "234319283049582";//invalid
+  String sample5 = "634319283049";//invalid
   
   @Test
   public void testCreditCardValidatorExists(){
     assertTrue(CreditCardValidator.isExist());
   }
 
+  @Test
+  public void testGetSumOfEvenPlacedDigit(){
+    int actual = CreditCardValidator.getSumOfEvenPlacedDigitDoubled(sample1);
+    int expected = 37;
+    assertEquals(actual, expected);
+  }
 
+  @Test
+  public void testGetSumOfOddPlacedDigit(){
+    int actual = CreditCardValidator.getSumOfOddPlacedDigit(sample1);
+    int expected = 38;
+    assertEquals(actual, expected);
+  }
+  
+  @Test
+  public void testIsNumberCombinationCorrect(){
+    boolean actual = CreditCardValidator.isNumberCombinationCorrect(sample1);
+    boolean expected = false;
+    assertEquals(actual, expected);
+    actual = CreditCardValidator.isNumberCombinationCorrect(sample2);
+    expected = true;
+    assertEquals(actual, expected);
+    actual = CreditCardValidator.isNumberCombinationCorrect(sample3);
+    expected = false;
+    assertEquals(actual, expected);
+  }
 
+  @Test
+  public void testIsLengthValid(){
+    assertTrue(CreditCardValidator.isLengthValid(sample1));
+    assertFalse(CreditCardValidator.isLengthValid(sample5));
+  }
 
+  @Test
+  public void testReturnCardType(){
+    String actual = CreditCardValidator.returnCardType(sample1);
+    String expected = "Visa";
+    assertEquals(actual, expected);
+    actual = CreditCardValidator.returnCardType(sample2);
+    expected = "MasterCard";
+    assertEquals(actual, expected);
+    actual = CreditCardValidator.returnCardType(sample5);
+    expected = "Discover";
+    assertEquals(actual, expected);
+  }
+  
+  @Test
+  public void testSumDigitsInTwoDigitNumber(){
+    int actual = CreditCardValidator.sumDigitsInTwoDigitNumber(18);
+    int expected = 9;
+    assertEquals(actual, expected);
+    actual = CreditCardValidator.sumDigitsInTwoDigitNumber(10);
+    expected = 1;
+    assertEquals(actual, expected);
+  }
 
-
+  @Test
+  public void testIsNumberDivisibleBy10(){
+    assertTrue(CreditCardValidator.isNumberDivisibleBy10(90));
+    assertFalse(CreditCardValidator.isNumberDivisibleBy10(34));
+  }
 
 }
