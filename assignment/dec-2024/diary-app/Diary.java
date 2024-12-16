99255eeab12f991bd4c8cf7555cfd80ca26d3784
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2024-12-16 03:26:33 +0100

    checkpoint: i want addEntry to return Entry object rather than boolean, the same may apply to updateEntry

--- assignment/dec-2024/diary-app/Diary.java
+++ assignment/dec-2024/diary-app/Diary.java
@@ -2,27 +2,63 @@ import java.util.ArrayList;
 
 public class Diary{
   public static boolean isExist(){ return true; }
+  private static int diaryCount = 0;
   
   private String name;
   private ArrayList<Entry> entries;
   private boolean islocked;
   
   public Diary(String name){
-    
-    
+    this.setName(name);
+    this.entries = new ArrayList<Entry>();
+    this.lock();
+    diaryCount += 1;
   }
   
   public String getName(){ return this.name; }
   public boolean getIsLocked(){ return this.islocked; }
   public ArrayList<Entry> getEntries(){ return this.entries; }
+  public static int getDiaryCount(){ return diaryCount; }
   
-  public void setName(){}
-  private void setIsLocked(boolean status){ this.islocked = status; }
+  public void unlock(){ this.islocked = false; }
+  public void lock(){ this.islocked = true; }
+  public void setName(String name){ this.name = name; }
   
-  public void unlock(){}
-  public void lock(){}
+  public boolean addEntry(String entrySubject, String entryBody){
+    if (getIsLocked()){ return false; }
+    else {
+      Entry newEntry = new Entry(entrySubject, entryBody);
+      return this.entries.add(newEntry);
+    }
+  }
+
+  public boolean updateEntry(Entry entry, String entrySubject, String entryBody){
+    //access the Arraylist
+    return false;
+  }
   
-  private
-  public String toString(){}
+  public boolean deleteEntry(Entry entry){
+    //access the array list
+    //entries.remove(entry)
+    return false;
+  }
+  
+  public Entry findEntryById(int id){
+    for(Entry entry : getEntries()){
+      if ( entry.getId() == id ){
+        return entry;
+      }
+    }
+    return null;
+  }
+  
+  
+  
+  
+  
+  
+  
+  private String toStringHelper(){ return ""; }
+  public String toString(){ return ""; }
 
 }
--- assignment/dec-2024/diary-app/TestDiaryApp.java
+++ assignment/dec-2024/diary-app/TestDiaryApp.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 public class TestDiaryApp{
   @Test 
   public void testEntryExists(){
@@ -9,19 +11,57 @@ public class TestDiaryApp{
   
   @Test
   public void testEntryConstructorCreatesANewEntry(){
-    assertEquals(Entry.getEntryCount(), 0);
+    int entryCountBeforeInstantiation = Entry.getEntryCount();
     String newEntrySubject = "Dec 15 entry";
     String newEntryBody = "They sent the wrong santa to Semicolon, he comes bearing snacks :)";
     Entry newEntry = new Entry(newEntrySubject, newEntryBody);
-    assertEquals(newEntry.getId(), 1);
+    assertEquals(newEntry.getId(), (entryCountBeforeInstantiation + 1));
     assertEquals(newEntry.getEntrySubject(), newEntrySubject);
     assertEquals(newEntry.getEntryBody(), newEntryBody);
-    assertEquals(Entry.getEntryCount(), 1);
+    assertEquals(Entry.getEntryCount(), (entryCountBeforeInstantiation + 1));
   }
   
   @Test 
   public void testDiaryExists(){
     assertTrue(Diary.isExist());
   }
-
+  
+  @Test
+  public void testDiaryConstructorCreatesANewDiary(){
+    int diaryCountBeforeInstantiation = Diary.getDiaryCount();
+    String diaryName = "Business";
+    Diary newDiary = new Diary(diaryName);
+    assertEquals(newDiary.getName(), diaryName);
+    assertEquals(newDiary.getEntries(), new ArrayList<Entry>());
+    assertEquals(newDiary.getDiaryCount(), (diaryCountBeforeInstantiation + 1));
+  }
+  
+  @Test
+  public void testDiaryCanNotAddEntryWhenLocked(){
+    String diaryName = "Personal Reflection";
+    Diary newDiary = new Diary(diaryName);
+    String subject = "Time and Money";
+    String body = "I think money or its abstraction should be a fundamental quantity like mass and time";
+    assertFalse(newDiary.addEntry(subject, body));    
+  }
+  
+  @Test
+  public void testDiaryCanAddEntryWhenUnlocked(){
+    String diaryName = "Personal Reflection";
+    Diary newDiary = new Diary(diaryName);
+    String subject = "Time and Money";
+    String body = "I think money or its abstraction should be a fundamental quantity like mass and time";
+    newDiary.unlock();
+    assertTrue(newDiary.addEntry(subject, body));    
+  }
+  
+  @Test
+  public void testDiaryCanFindEntryById(){
+    Entry actual = findEntryById(1);
+    assertNotNull(actual);
+  }
+  
+  
+  
+  
 }
