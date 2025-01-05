89643698a51a015e9ff02f8021660bc352ec2a32
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-01-05 19:58:35 +0100

    Made TurtleGraphics more modular, extracted a Main class form TurtleGraphics.java. with this i could write more test for TurtleGraphics.java

---
+++ assignment/dec-2024/turtle-graphics/src/Main.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        TurtleGraphics turtleGraphics = new TurtleGraphics();
+
+        final String instructionSet = """
+                Command |  Meaning
+                - - - - + - - - - -
+                1       |  Pen up
+                2       |  Pen down
+                3       |  Turn right
+                4       |  Turn left
+                5,N     |  Move forward N spaces (replace N for a number)
+                6       |  Display the 20-by-20 array
+                9       |  End of data (sentinel)
+                <Separate commands by new line>
+                """;
+
+        System.out.println(instructionSet);
+
+        boolean sentinel = true;
+        while (sentinel) {
+            String command = input.nextLine();
+            switch (command) {
+                case "1", "2", "3", "4" -> {
+                    colourCommandTextGreen(command);
+                    turtleGraphics.parser(command);
+                }
+                case "6" -> {
+                    colourCommandTextGreen(command);
+                    System.out.println(turtleGraphics.displayGraphics());
+                }
+                case "9" -> {
+                    colourCommandTextGreen(command);
+                    sentinel = false;
+                }
+                default -> {
+                    String[] temp = command.split(",");
+                    if ((temp.length == 2) && temp[0].equals("5") && (Integer.parseInt(temp[1]) >= 1)) {
+                        colourCommandTextGreen(command);
+                        turtleGraphics.parser(command);
+                    } else {
+                        System.out.print("\033[A\033[K");
+                    }
+                }
+            }
+        }
+    }
+
+    private static void colourCommandTextGreen(String command) {
+        System.out.print("\033[A\033[K");
+        System.out.println("\033[32m" + command + "\033[0m");
+    }
+}
--- assignment/dec-2024/turtle-graphics/src/TurtleGraphics.java
+++ assignment/dec-2024/turtle-graphics/src/TurtleGraphics.java
@@ -1,67 +1,19 @@
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class TurtleGraphics {
-    public static void main(String[] args) {
+    final String[][] floor;
+    final Turtle     turtle;
+    
+    public TurtleGraphics() {
+        this.floor = new String[20][20];
+        this.turtle = new Turtle();
 
-        final String[][] floor = new String[20][20];
-        final Turtle turtle = new Turtle();
-
-        for (String[] row : floor) {
+        for (String[] row : this.floor) {
             Arrays.fill(row, "0");
         }
-
-        final String instructionSet = """
-                Command |  Meaning
-                - - - - + - - - - -
-                1       |  Pen up
-                2       |  Pen down
-                3       |  Turn right
-                4       |  Turn left
-                5,N     |  Move forward N spaces (replace N for a number)
-                6       |  Display the 20-by-20 array
-                9       |  End of data (sentinel)
-                <Separate commands by new line>
-                """;
-
-        System.out.println(instructionSet);
-        Scanner input = new Scanner(System.in);
-
-        boolean sentinel = true;
-        while (sentinel) {
-            String command = input.nextLine();
-            switch (command) {
-                case "1", "2", "3", "4" -> {
-                    colourCommandTextGreen(command);
-                    parser(command, turtle, floor);
-                }
-                case "6" -> {
-                    colourCommandTextGreen(command);
-                    System.out.println(displayGraphics(floor));
-                }
-                case "9" -> {
-                    colourCommandTextGreen(command);
-                    sentinel = false;
-                }
-                default -> {
-                    String[] temp = command.split(",");
-                    if ((temp.length == 2) && temp[0].equals("5") && (Integer.parseInt(temp[1]) >= 1)) {
-                        colourCommandTextGreen(command);
-                        parser(command, turtle, floor);
-                    } else {
-                        System.out.print("\033[A\033[K");
-                    }
-                }
-            }
-        }
     }
-
-    private static void colourCommandTextGreen(String command) {
-        System.out.print("\033[A\033[K");
-        System.out.println("\033[32m" + command + "\033[0m");
-    }
-
-    private static String displayGraphics(String[][] floor) {
+    
+    public String displayGraphics() {
         StringBuilder output = new StringBuilder();
         for (String[] row : floor) {
             output.append(
@@ -71,7 +23,7 @@ public class TurtleGraphics {
         return output.toString();
     }
 
-    private static void parser(String command, Turtle turtle, String[][] floor) {
+    public void parser(String command) {
         switch (command) {
             case "1" -> turtle.penUP();
             case "2" -> turtle.penDOWN();
@@ -79,12 +31,12 @@ public class TurtleGraphics {
             case "4" -> turtle.turnLEFT();
             default -> {
                 int noOfSpaces = Integer.parseInt(command.split(",")[1]);
-                moveForward(noOfSpaces, turtle, floor);
+                moveForward(noOfSpaces);
             }
         }
     }
 
-    private static void moveForward(int noOfSpaces, Turtle turtle, String[][] floor) {
+    private void moveForward(int noOfSpaces) {
         Turtle.PenState penState = turtle.getPenState();
         Turtle.Orientation orientation = turtle.getOrientation();
         int currentPosX = turtle.getX();
