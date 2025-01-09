d85241813962b9fa754c2e235110eb8e8673f6a4
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-01-09 15:10:49 +0100

    Importing the Diary project into IntelliJ IDEA

--- assignment/dec-2024/diary-app/DiaryApp.java
+++ assignment/dec-2024/diary-app/DiaryApp.java
@@ -1,51 +1,44 @@
 import java.util.Scanner;
 
-public class DiaryApp{
-  Scanner input = new Scanner(System.in);
-  
-  public static void main(String[] args){
-    String welcomeMessage = """
-    Diary App
-    (Non-AI powered journaling app)""";
-    
-    mainMenu();
-  }
-  
-  public static void mainMenu(){
-    String mainMenu = """
-    MainMenu:
-    1. Create New Diary
-    2. Open My Diaries
-    3. Exit
-    """;
-    
-    boolean isJournaling = true;
-    while(isJournaling){
-      System.out.print(mainMenu);
-      int choice = input.nextInt();
-      switch(choice){
-        case 1:
-          
-          break;
-        case 2:
-          
-          break;
-        case 0:
-          isJournaling = false;
-          break;
-        default:
-          
-      }
-    
-    
-    
+public class DiaryApp {
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String welcomeMessage = """
+                Diary App
+                (Non-AI powered journaling app)""";
+
+        mainMenu();
     }
-  }
-    
-  }
+
+    public static void mainMenu() {
+        String mainMenu = """
+                MainMenu:
+                1. Create New Diary
+                2. Open My Diaries
+                3. Exit
+                """;
+
+        boolean isJournaling = true;
+        while (isJournaling) {
+            System.out.print(mainMenu);
+            int choice = input.nextInt();
+            switch (choice) {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 0:
+                    isJournaling = false;
+                    break;
+                default:
+
+            }
 
 
-
-
+        }
+    }
 
 }
