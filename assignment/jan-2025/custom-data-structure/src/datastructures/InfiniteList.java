c6bfcb533f010743946039542f6ccfdf7a8fbd82
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-01-22 07:17:59 +0100

    checkpoint: working on list data structure

--- assignment/jan-2025/custom-data-structure/src/datastructures/InfiniteList.java
+++ assignment/jan-2025/custom-data-structure/src/datastructures/InfiniteList.java
@@ -1,4 +1,66 @@
 package datastructures;
 
+
+import java.util.stream.IntStream;
+
 public class InfiniteList {
+    private int count = 0;
+    private int capacity = 5;
+    private String[] infiniteList = new String[capacity];
+
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    public void add(String element) {
+        ensureCapacity();
+        infiniteList[count] = element;
+        count++;
+    }
+
+    private void ensureCapacity() {
+        if (count == capacity) {
+            int newCapacity = count * 2;
+            String[] newList = new String[newCapacity];
+            IntStream.range(0, newCapacity).forEach(i -> newList[i] = infiniteList[i]);
+            infiniteList = newList;
+            capacity = newCapacity;
+        }
+    }
+
+    public int size() {
+        return count;
+    }
+
+    public boolean contains(String element) {
+        return IntStream.range(0, count).anyMatch(index -> element.equals(infiniteList[index]));
+    }
+
+    public void remove(String element) {
+        int elementIndex = indexOf(element);
+        remove(elementIndex);
+    }
+
+    private void remove(int elementIndex) {
+        if (elementIndex != -1) {
+            for (int index = elementIndex + 1; index < count; index++) {
+                infiniteList[index] = infiniteList[index - 1];
+            }
+            count--;
+        }
+    }
+
+    private int indexOf(String element) {
+        for (int index = 0; index < count; index++) {
+            if (infiniteList[index].equals(element)){
+                return index;
+            }
+        }
+        return -1;
+    }
+
+
+    public String get(int index) {
+        return infiniteList[index];
+    }
 }
--- assignment/jan-2025/custom-data-structure/test/datastructures/TestInfiniteList.java
+++ assignment/jan-2025/custom-data-structure/test/datastructures/TestInfiniteList.java
@@ -1,5 +1,61 @@
 package datastructures;
 
-public class TestInfiniteList {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestInfiniteList {
+    InfiniteList infiniteList;
+
+    @BeforeEach
+    void setUp() {
+        infiniteList = new InfiniteList();
+    }
+
+    @Test
+    public void testInfiniteList_isEmpty() {
+        assertTrue(infiniteList.isEmpty());
+    }
+
+    @Test
+    public void testInfiniteList_hasCapacityToStoreNewElement(){
+        infiniteList.add("hello");
+        infiniteList.add("world");
+        assertFalse(infiniteList.isEmpty());
+        assertEquals(2, infiniteList.size());
+        infiniteList.add("Belle");
+        assertEquals(3, infiniteList.size());
+    }
+
+    @Test
+    public void testInfiniteList_storesElementInList(){
+        assertFalse(infiniteList.contains("Terve"));
+        assertFalse(infiniteList.contains("world"));
+        infiniteList.add("Terve");
+        infiniteList.add("world");
+        assertTrue(infiniteList.contains("Terve"));
+        assertTrue(infiniteList.contains("world"));
+    }
+
+    @Test
+    public void testInfiniteList_removeElementFromList(){
+        infiniteList.add("Terve");
+        infiniteList.add("kaunis");
+        infiniteList.add("nainen");
+        assertEquals(3, infiniteList.size());
+        assertTrue(infiniteList.contains("nainen"));
+        infiniteList.remove("nainen");
+        assertFalse(infiniteList.contains("nainen"));
+        assertEquals(2, infiniteList.size());
+    }
+
+    @Test
+    public void testInfiniteList_canFetchElementFromList(){
+        infiniteList.add("Nigeria");
+        infiniteList.add("kaunis");
+        infiniteList.add("maa");
+        assertEquals("kaunis", infiniteList.get(1));
+        assertEquals("Nigeria", infiniteList.get(0));
+    }
 }
