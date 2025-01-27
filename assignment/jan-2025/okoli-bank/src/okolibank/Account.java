dfcbd4fad2a60f3a665b1f3a2a889750530d147b
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-01-27 22:06:15 +0100

    completed Account and TestAccount

---
+++ assignment/jan-2025/okoli-bank/src/okolibank/Account.java
@@ -0,0 +1,70 @@
+package okolibank;
+
+public class Account {
+    private final int accountNumber;
+    private String firstName;
+    private String lastName;
+    private String pin;
+    private int balance;
+
+    public Account(int accountNumber, String firstName, String lastName, String pin) {
+        this.accountNumber = accountNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.pin = pin;
+    }
+
+    public boolean exist() {
+        return true;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void updateFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void updateLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void updatePin(String oldPin, String newPin) {
+        if (pin.equals(oldPin)){
+            this.pin = newPin;
+        } else throw new IllegalArgumentException("Pins don't match");
+    }
+
+    public int checkBalance(String pin) {
+        if (pin.equals(this.pin)){
+            return balance;
+        } else throw new IllegalArgumentException("Pins don't match");
+    }
+
+    public void deposit(int amount) {
+        if (amount > 0){
+            balance += amount;
+        } else throw new IllegalArgumentException("Amount can't be zero or negative");
+    }
+
+    public void withdraw(int amount, String pin) {
+        if (pin.equals(this.pin)){
+            if (amount > balance || amount < 0){
+                throw new IllegalArgumentException(
+                        "Withdrawal amount must be less then account balance and greater than zero");
+            } else {balance -= amount;}
+        } else throw new IllegalArgumentException("Pins don't match");
+    }
+
+
+
+}
---
+++ assignment/jan-2025/okoli-bank/test/okolibank/TestAccount.java
@@ -0,0 +1,95 @@
+package okolibank;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestAccount {
+    private Account account;
+
+    @BeforeEach
+    void setUp() {
+        account = new Account(100, "Ifeanyi", "Cyriacus", "0000");
+    }
+
+    @Test
+    public void testAccount_exist(){
+        assertTrue(account.exist());
+    }
+
+    @Test
+    public void testAccount_canBeCreated(){
+        assertEquals(100, account.getAccountNumber());
+        assertEquals("Ifeanyi", account.getFirstName());
+        assertEquals("Cyriacus", account.getLastName());
+    }
+    
+    @Test
+    public void testAccount_canBeUpdated(){
+        account.updateFirstName("Valentine");
+        assertEquals("Valentine", account.getFirstName());
+        account.updateLastName("Mark");
+        assertEquals("Mark", account.getLastName());
+    }
+
+    @Test
+    public void testAccount_balanceIsSetToZeroAtCreation(){
+        assertEquals(0, account.checkBalance("0000"));
+    }
+
+    @Test
+    public void testAccount_pinCanOnlyBeUpdatedWhenTheOldPinIsCorrect(){
+        assertThrows(IllegalArgumentException.class,
+                () -> account.updatePin("0110", "1234"));
+    }
+
+    @Test
+    public void testAccount_pinUpdateWorks(){
+        assertEquals(0, account.checkBalance("0000"));
+        account.updatePin("0000", "1234");
+        assertEquals(0, account.checkBalance("1234"));
+    }
+
+    @Test
+    public void testAccount_canOnlyCheckBalanceWhenPinIsCorrect(){
+        assertThrows(IllegalArgumentException.class,
+                () -> account.checkBalance("0110"));
+        assertEquals(0, account.checkBalance("0000"));
+    }
+
+    @Test
+    public void testAccount_supportsDeposit(){
+        account.deposit(30_000);
+        assertEquals(30_000, account.checkBalance("0000"));
+        account.deposit(300);
+        account.deposit(400);
+        assertEquals(30_700, account.checkBalance("0000"));
+    }
+
+    @Test
+    public void testAccount_throwExceptionForInvalidDepositAmount(){
+        assertThrows(IllegalArgumentException.class, ()-> account.deposit(-300));
+        assertEquals(0, account.checkBalance("0000"));
+    }
+
+    @Test
+    public void testAccount_supportsWithdrawal(){
+        account.deposit(30_000);
+        account.withdraw(10_000, "0000");
+        assertEquals(20_000, account.checkBalance("0000"));
+    }
+
+    @Test
+    public void testAccount_throwExceptionForInvalidWithdrawAmount(){
+        account.deposit(30_000);
+        assertThrows(IllegalArgumentException.class, ()-> account.withdraw(-200, "0000"));
+        assertThrows(IllegalArgumentException.class, ()-> account.withdraw(40_000, "0000"));
+    }
+
+    @Test
+    public void testAccount_throwExceptionForIncorrectPin(){
+        account.deposit(30_000);
+        assertThrows(IllegalArgumentException.class, ()-> account.withdraw(300, "0400"));
+    }
+}
