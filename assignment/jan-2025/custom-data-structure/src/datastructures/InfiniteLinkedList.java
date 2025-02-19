fb0f497a0a05a9bb0ea6615a6e7fcbf18162d7ad
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-02-19 22:40:59 +0100

    my linkedList attempt in progress

--- assignment/jan-2025/custom-data-structure/src/datastructures/InfiniteLinkedList.java
+++ assignment/jan-2025/custom-data-structure/src/datastructures/InfiniteLinkedList.java
@@ -1,10 +1,79 @@
 package datastructures;
 
 public class InfiniteLinkedList {
-    public boolean isEmpty() {
-        return true;
+    class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+
+        public void append(Node node) {
+            if (this.next == null) {
+                this.next = node;
+            } else {
+                this.next.append(node);
+            }
+        }
+
+
     }
+
+    private Node head;
+
+    public InfiniteLinkedList() {
+        this.head = null;
+    }
+
+    public void prepend(int data) {
+        Node newNode = new Node(data);
+        newNode.next = this.head;
+        this.head = newNode;
+    }
+
+    public void append(Node newNode) {
+        if (this.head == null) {
+            this.head = newNode;
+        } else {
+            this.head.append(newNode);
+        }
+    }
+
+    public void insert(int data) {
+        Node newNode = new Node(data);
+        newNode.next = this.head;
+        this.head = newNode;
+    }
+
+    public Node getHead() {
+        return this.head;
+    }
+
+    public int getLength() {
+        int length = 0;
+        for (Node current = this.head; current != null; current = current.next) {
+            length++;
+        } return length;
+    }
+
+    public boolean isEmpty() {
+        return this.head == null;
+    }
+
+    public void print() {
+        Node current = this.head;
+        while (current != null) {
+            System.out.print("[" + current.data + "] -> ");
+            current = current.next;
+        }
+        System.out.println("[NULL]");
+    }
+
 }
+
+
 /*
 * list + queue pro max
 * ==> list + deque
