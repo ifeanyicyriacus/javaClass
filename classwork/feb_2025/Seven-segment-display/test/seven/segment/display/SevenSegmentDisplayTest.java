412dfefdb3cd8a6da2a62a4137b62f7272be0cba
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-02-24 00:49:06 +0100

    Small progress

--- classwork/feb_2025/Seven-segment-display/test/seven/segment/display/SevenSegmentDisplayTest.java
+++ classwork/feb_2025/Seven-segment-display/test/seven/segment/display/SevenSegmentDisplayTest.java
@@ -3,40 +3,110 @@ package seven.segment.display;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SevenSegmentDisplayTest {
     SevenSegmentDisplay display;
+
+    final String ZERO_PROMPT = "11111101";
+    final String[][] ZERO = {
+            {"#", "#", "#", "#"},
+            {"#", " ", " ", "#"},
+            {"#", " ", " ", "#"},
+            {"#", " ", " ", "#"},
+            {"#", "#", "#", "#"},
+    };
+
+    final String ONE_PROMPT = "";
+    final String[][] ONE = {
+            {" ", " ", " ", "#"},
+            {" ", " ", " ", "#"},
+            {" ", " ", " ", "#"},
+            {" ", " ", " ", "#"},
+            {" ", " ", " ", "#"},
+    };
+
+    final String TWO_PROMPT = "";
+    final String[][] TWO = {
+            {"#", "#", "#", "#"},
+            {" ", " ", " ", "#"},
+            {"#", "#", "#", "#"},
+            {"#", " ", " ", " "},
+            {"#", "#", "#", "#"},
+    };
+
+    final String THREE_PROMPT = "";
+    final String[][] THREE = {
+            {"#", "#", "#", "#"},
+            {" ", " ", " ", "#"},
+            {"#", "#", "#", "#"},
+            {" ", " ", " ", "#"},
+            {"#", "#", "#", "#"},
+    };
+
+    final String FOUR_PROMPT = "";
+    final String[][] FOUR = {
+            {"#", " ", " ", "#"},
+            {"#", " ", " ", "#"},
+            {"#", "#", "#", "#"},
+            {" ", " ", " ", "#"},
+            {" ", " ", " ", "#"},
+    };
+
+    final String FIVE_PROMPT = "";
+    final String[][] FIVE = {
+            {"#", "#", "#", "#"},
+            {"#", " ", " ", " "},
+            {"#", "#", "#", "#"},
+            {" ", " ", " ", "#"},
+            {"#", "#", "#", "#"},
+    };
+
+    final String SIX_PROMPT = "";
+    final String[][] SIX = {
+            {"#", "#", "#", "#"},
+            {"#", " ", " ", " "},
+            {"#", "#", "#", "#"},
+            {"#", " ", " ", "#"},
+            {"#", "#", "#", "#"},
+    };
+
+    final String SEVEN_PROMPT = "";
+    final String[][] SEVEN = {
+            {"#", "#", "#", "#"},
+            {" ", " ", " ", "#"},
+            {" ", " ", " ", "#"},
+            {" ", " ", " ", "#"},
+            {" ", " ", " ", "#"},
+    };
+
+    final String EIGHT_PROMPT = "";
+    final String[][] EIGHT = {
+            {"#", "#", "#", "#"},
+            {"#", " ", " ", "#"},
+            {"#", "#", "#", "#"},
+            {"#", " ", " ", "#"},
+            {"#", "#", "#", "#"},
+    };
+
+    final String NINE_PROMPT = "";
+    final String[][] NINE = {
+            {"#", "#", "#", "#"},
+            {"#", " ", " ", "#"},
+            {"#", "#", "#", "#"},
+            {" ", " ", " ", "#"},
+            {" ", " ", " ", "#"},
+    };
+
+
+
+
+
     @BeforeEach
     public void setUp() {
         display = new SevenSegmentDisplay();
     }
 
     @Test
-    public void testDisplayDoesNotDisplayAnythingAfterCreation() {
-//        String expected = """
-//                ####
-//                #  #
-//                ####
-//                #  #
-//                ####
-//                """;
 
-        String expected = """
-                    
-                    
-                        
-                    
-                """;
-        String actual  = display.toString();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testReceiveInputReturnsAnArrayOfIntBetween0and1ForCorrectInput() {
-//        dei
-
-    }
 
 }
