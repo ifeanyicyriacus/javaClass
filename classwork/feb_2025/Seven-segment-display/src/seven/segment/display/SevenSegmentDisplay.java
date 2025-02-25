32f2abafe9f267ac223f6d1ceb42e276a3c91fb0
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-02-25 02:55:13 +0100

    SevenSegmentDisplay task completed

--- classwork/feb_2025/Seven-segment-display/src/seven/segment/display/SevenSegmentDisplay.java
+++ classwork/feb_2025/Seven-segment-display/src/seven/segment/display/SevenSegmentDisplay.java
@@ -1,4 +1,100 @@
 package seven.segment.display;
 
+import java.util.Arrays;
+
 public class SevenSegmentDisplay {
+
+    private final String[][] display = new String[5][4];
+
+    private final String token = "#";
+
+    public void enterPrompt(String prompt) {
+        setSegmentsValue(prompt);
+    }
+
+    private void setSegmentsValue(String prompt) {
+        var promptChar = prompt.split("");
+        setDisplayBlank();
+        setA(promptChar[0].equals("1"));
+        setB(promptChar[1].equals("1"));
+        setC(promptChar[2].equals("1"));
+        setD(promptChar[3].equals("1"));
+        setE(promptChar[4].equals("1"));
+        setF(promptChar[5].equals("1"));
+        setG(promptChar[6].equals("1"));
+        toggleDisplayOnOrOff(promptChar[7].equals("1"));
+    }
+
+    private void setA(boolean state) {
+        display[0][0] = state ? token: display[0][0];
+        display[0][1] = state ? token: display[0][1];
+        display[0][2] = state ? token: display[0][2];
+        display[0][3] = state ? token: display[0][3];
+    }
+
+    private void setB(boolean state) {
+        display[0][3] = state ? token: display[0][3];
+        display[1][3] = state ? token: display[1][3];
+        display[2][3] = state ? token: display[2][3];
+    }
+
+    private void setC(boolean state) {
+        display[2][3] = state ? token: display[2][3];
+        display[3][3] = state ? token: display[3][3];
+        display[4][3] = state ? token: display[4][3];
+    }
+
+    private void setD(boolean state) {
+        display[4][3] = state ? token: display[4][3];
+        display[4][2] = state ? token: display[4][2];
+        display[4][1] = state ? token: display[4][1];
+        display[4][0] = state ? token: display[4][0];
+    }
+
+    private void setE(boolean state) {
+        display[4][0] = state ? token: display[4][0];
+        display[3][0] = state ? token: display[3][0];
+        display[2][0] = state ? token: display[2][0];
+    }
+
+    private void setF(boolean state) {
+        display[2][0] = state ? token: display[2][0];
+        display[1][0] = state ? token: display[1][0];
+        display[0][0] = state ? token: display[0][0];
+    }
+
+    private void setG(boolean state) {
+        display[2][0] = state ? token: display[2][0];
+        display[2][1] = state ? token: display[2][1];
+        display[2][2] = state ? token: display[2][2];
+        display[2][3] = state ? token: display[2][3];
+    }
+
+    private void toggleDisplayOnOrOff(boolean state) {
+        if (!state) {
+            setDisplayBlank();
+        }
+    }
+
+    private void setDisplayBlank() {
+        for (String[] strings : display) {
+            Arrays.fill(strings, " ");
+        }
+    }
+
+    public String[][] getDisplay() {
+        return display;
+    }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        for (String[] rows : display) {
+            for (String cell : rows) {
+                sb.append(cell);
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
 }
--- classwork/feb_2025/Seven-segment-display/test/seven/segment/display/SevenSegmentDisplayTest.java
+++ classwork/feb_2025/Seven-segment-display/test/seven/segment/display/SevenSegmentDisplayTest.java
@@ -3,103 +3,99 @@ package seven.segment.display;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 
 public class SevenSegmentDisplayTest {
     SevenSegmentDisplay display;
+    final String ZERO_PROMPT    = "11111101";
+    final String ONE_PROMPT     = "01100001";
+    final String TWO_PROMPT     = "11011011";
+    final String THREE_PROMPT   = "11110011";
+    final String FOUR_PROMPT    = "01100111";
+    final String FIVE_PROMPT    = "10110111";
+    final String SIX_PROMPT     = "10111111";
+    final String SEVEN_PROMPT   = "11100001";
+    final String EIGHT_PROMPT   = "11111111";
+    final String NINE_PROMPT    = "11100111";
+    final String BLANK_PROMPT   = "11110110";
 
-    final String ZERO_PROMPT = "11111101";
     final String[][] ZERO = {
             {"#", "#", "#", "#"},
             {"#", " ", " ", "#"},
             {"#", " ", " ", "#"},
             {"#", " ", " ", "#"},
-            {"#", "#", "#", "#"},
-    };
+            {"#", "#", "#", "#"},};
 
-    final String ONE_PROMPT = "";
     final String[][] ONE = {
             {" ", " ", " ", "#"},
             {" ", " ", " ", "#"},
             {" ", " ", " ", "#"},
             {" ", " ", " ", "#"},
-            {" ", " ", " ", "#"},
-    };
+            {" ", " ", " ", "#"},};
 
-    final String TWO_PROMPT = "";
     final String[][] TWO = {
             {"#", "#", "#", "#"},
             {" ", " ", " ", "#"},
             {"#", "#", "#", "#"},
             {"#", " ", " ", " "},
-            {"#", "#", "#", "#"},
-    };
+            {"#", "#", "#", "#"},};
 
-    final String THREE_PROMPT = "";
     final String[][] THREE = {
             {"#", "#", "#", "#"},
             {" ", " ", " ", "#"},
             {"#", "#", "#", "#"},
             {" ", " ", " ", "#"},
-            {"#", "#", "#", "#"},
-    };
+            {"#", "#", "#", "#"},};
 
-    final String FOUR_PROMPT = "";
     final String[][] FOUR = {
             {"#", " ", " ", "#"},
             {"#", " ", " ", "#"},
             {"#", "#", "#", "#"},
             {" ", " ", " ", "#"},
-            {" ", " ", " ", "#"},
-    };
+            {" ", " ", " ", "#"},};
 
-    final String FIVE_PROMPT = "";
     final String[][] FIVE = {
             {"#", "#", "#", "#"},
             {"#", " ", " ", " "},
             {"#", "#", "#", "#"},
             {" ", " ", " ", "#"},
-            {"#", "#", "#", "#"},
-    };
+            {"#", "#", "#", "#"},};
 
-    final String SIX_PROMPT = "";
     final String[][] SIX = {
             {"#", "#", "#", "#"},
             {"#", " ", " ", " "},
             {"#", "#", "#", "#"},
             {"#", " ", " ", "#"},
-            {"#", "#", "#", "#"},
-    };
+            {"#", "#", "#", "#"},};
 
-    final String SEVEN_PROMPT = "";
     final String[][] SEVEN = {
             {"#", "#", "#", "#"},
             {" ", " ", " ", "#"},
             {" ", " ", " ", "#"},
             {" ", " ", " ", "#"},
-            {" ", " ", " ", "#"},
-    };
+            {" ", " ", " ", "#"},};
 
-    final String EIGHT_PROMPT = "";
     final String[][] EIGHT = {
             {"#", "#", "#", "#"},
             {"#", " ", " ", "#"},
             {"#", "#", "#", "#"},
             {"#", " ", " ", "#"},
-            {"#", "#", "#", "#"},
-    };
+            {"#", "#", "#", "#"},};
 
-    final String NINE_PROMPT = "";
     final String[][] NINE = {
             {"#", "#", "#", "#"},
             {"#", " ", " ", "#"},
             {"#", "#", "#", "#"},
             {" ", " ", " ", "#"},
-            {" ", " ", " ", "#"},
-    };
+            {" ", " ", " ", "#"},};
 
-
-
-
+    final String[][] BLANK = {
+            {" ", " ", " ", " "},
+            {" ", " ", " ", " "},
+            {" ", " ", " ", " "},
+            {" ", " ", " ", " "},
+            {" ", " ", " ", " "},};
 
     @BeforeEach
     public void setUp() {
@@ -107,6 +103,90 @@ public class SevenSegmentDisplayTest {
     }
 
     @Test
+    public void testDisplayIsBlankWhenBlankPromptIsGiven() {
+        display.enterPrompt(BLANK_PROMPT);
+        String[][] actual = display.getDisplay();
+        System.out.println(display);
+        assertArrayEquals(BLANK, actual);
+    }
 
+    @Test
+    public void testDisplayIsZeroWhenZeroPromptIsGiven() {
+        display.enterPrompt(ZERO_PROMPT);
+        String[][] actual = display.getDisplay();
+        System.out.println(display);
+        assertArrayEquals(ZERO, actual);
+    }
 
+    @Test
+    public void testDisplayIsOneWhenOnePromptIsGiven() {
+        display.enterPrompt(ONE_PROMPT);
+        String[][] actual = display.getDisplay();
+        System.out.println(display);
+        assertArrayEquals(ONE, actual);
+    }
+
+    @Test
+    public void testDisplayIsTwoWhenTwoPromptIsGiven() {
+        display.enterPrompt(TWO_PROMPT);
+        String[][] actual = display.getDisplay();
+        System.out.println(display);
+        assertArrayEquals(TWO, actual);
+    }
+
+    @Test
+    public void testDisplayIsThreeWhenThreePromptIsGiven() {
+        display.enterPrompt(THREE_PROMPT);
+        String[][] actual = display.getDisplay();
+        System.out.println(display);
+        assertArrayEquals(THREE, actual);
+    }
+
+    @Test
+    public void testDisplayIsFourWhenFourPromptIsGiven() {
+        display.enterPrompt(FOUR_PROMPT);
+        String[][] actual = display.getDisplay();
+        System.out.println(display);
+        assertArrayEquals(FOUR, actual);
+    }
+
+    @Test
+    public void testDisplayIsFiveWhenFivePromptIsGiven() {
+        display.enterPrompt(FIVE_PROMPT);
+        String[][] actual = display.getDisplay();
+        System.out.println(display);
+        assertArrayEquals(FIVE, actual);
+    }
+
+    @Test
+    public void testDisplayIsSixWhenSixPromptIsGiven() {
+        display.enterPrompt(SIX_PROMPT);
+        String[][] actual = display.getDisplay();
+        System.out.println(display);
+        assertArrayEquals(SIX, actual);
+    }
+
+    @Test
+    public void testDisplayIsSevenWhenSevenPromptIsGiven() {
+        display.enterPrompt(SEVEN_PROMPT);
+        String[][] actual = display.getDisplay();
+        System.out.println(display);
+        assertArrayEquals(SEVEN, actual);
+    }
+
+    @Test
+    public void testDisplayIsEightWhenEightPromptIsGiven() {
+        display.enterPrompt(EIGHT_PROMPT);
+        String[][] actual = display.getDisplay();
+        System.out.println(display);
+        assertArrayEquals(EIGHT, actual);
+    }
+
+    @Test
+    public void testDisplayIsNineWhenNinePromptIsGiven() {
+        display.enterPrompt(NINE_PROMPT);
+        String[][] actual = display.getDisplay();
+        System.out.println(display);
+        assertArrayEquals(NINE, actual);
+    }
 }
